e9436f14fb10604bd31fa6f12c191fc9ba80f7f8
Kingsly <b99b01078@ntu.edu.tw>
2013-12-02 22:03:36 -0500

    Make file location organized

---
+++ trial/php_trial.php
@@ -0,0 +1,14 @@
+<?php
+
+$conn = new mysqli('cs.unc.edu', 'cchunhao', 'abroad#1', 'database');
+
+//$result = $conn->query(queryString);
+
+$yql_base_url = "http://query.yahooapis.com/v1/public/yql";
+
+$yql_query = ""
+
+http://d.yimg.com/autoc.finance.yahoo.com/
+autoc?query=micro&callback=YAHOO.Finance.SymbolSuggest.ssCallback
+
+?>
